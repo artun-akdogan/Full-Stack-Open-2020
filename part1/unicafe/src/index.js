e33f45e27dfaf8ab9611e390626d5ea08d3d6c94
artun-akdogan <artun.akdogan@boun.edu.tr>
2020-03-31 06:36:06 +0300

    Add code for exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Button = ({func, txt}) =><button onClick={func}>{txt}</button>
 
-  return (
-    <div>
-      code here
-    </div>
-  )
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    return (
+        <div>
+            <div>
+                <h1>give feedback</h1>
+                <Button func={()=>setGood(good+1)} txt='good' />
+                <Button func={()=>setNeutral(neutral+1)} txt='neutral' />
+                <Button func={()=>setBad(bad+1)} txt='bad' />
+            </div>
+            <div>
+                <h1>statistics</h1>
+                <p>good {good}</p>
+                <p>neutral {neutral}</p>
+                <p>bad {bad}</p>
+            </div>
+        </div>
+    )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+ReactDOM.render(<App />,
+    document.getElementById('root')
 )
