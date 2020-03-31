97944418a2d202dd9a332e98e624042f0dfcad72
artun-akdogan <artun.akdogan@boun.edu.tr>
2020-03-31 06:25:55 +0300

    Initialize unicafe starting point code

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
