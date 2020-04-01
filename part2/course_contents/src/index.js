d261053ea7d3753b394a9da5abaf972539152046
artun-akdogan <artun.akdogan@boun.edu.tr>
2020-04-01 07:44:52 +0300

    Fix code for exercise 2.1

--- part2/course_contents/src/index.js
+++ part2/course_contents/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({name}) =>{
+  return <h1>{name}</h1>
+}
+
+const Part = ({props}) =>{
+  return <p>{props.name} {props.exercises}</p>
+}
+
+const Content = ({parts}) =>{
+  //Added "React.Fragment" to eliminate "Each child in a list should have a unique "key" prop" warning.
+  return (
+    <div>
+      {parts.map((val, id)=><Part key={id}  props={val} />)}
+    </div>
+  )
+}
+
+const Course = ({course})=>(
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+  </div>
+)
+
 const App = () => {
   const course = {
     id: 1,
