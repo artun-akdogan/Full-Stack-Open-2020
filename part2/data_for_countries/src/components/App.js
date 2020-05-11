5dc34d64ecbd84d931cb47063a2e956450def4f9
artun-akdogan <artun.akdogan@boun.edu.tr>
2020-05-11 20:12:54 +0300

    Initialize data_for_countries

---
+++ part2/data_for_countries/src/components/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const App = () =>{
+    return (
+        <div>
+            find countries <input />
+        </div>
+    )
+}
+
+export default App
