36b9e026d711ce9e1f30b5d2b8ec81a4f38d84a3
artun-akdogan <artun.akdogan@boun.edu.tr>
2020-05-11 21:23:43 +0300

    Fix filter case sensitive bug

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Persons = ({ persons, filterName }) => (
     <div>
-        {persons.filter(val => val.name.includes(filterName)).map((person => <div key={person.name}>{person.name} {person.number}</div>))}
+        {persons.filter(val => val.name.toLowerCase().includes(filterName.toLowerCase())).map((person => <div key={person.name}>{person.name} {person.number}</div>))}
     </div>
 )
 
