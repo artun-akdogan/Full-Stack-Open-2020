232b76b7196203fd672fb99eb4b72cc4bdefad35
artun-akdogan <artun.akdogan@boun.edu.tr>
2020-05-12 05:36:08 +0300

    Add code for exercise 2.14

--- part2/data_for_countries/src/components/App.js
+++ part2/data_for_countries/src/components/App.js
@@ -1,12 +1,30 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
+const Weather = ({city}) => {
+    const [weather,setWeather] = useState({})
+    useEffect(() => {
+    axios.get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${city}`).then(response => {
+        setWeather(response.data)
+    })}, [])
+    if(Object.keys(weather).length !== 0 && weather.success!==false)
+    return (
+        <div>
+            <h2>Weather in {city}</h2>
+            <p><b>temperature:</b> {weather.current.temperature} Celcius</p>
+            <img src={weather.current.weather_icons} alt={weather.current.weather_descriptions}/>
+            <p><b>wind:</b> {weather.current.wind_speed} mph direction {weather.current.wind_dir}</p>
+        </div>
+    )
+    else return <div></div>
+}
+
 const App = () =>{
     const [countries, setCountries] = useState([])
     const [filterName, setFilterName] = useState('')
     const countryList = countries.filter(val => val.name.toLowerCase().includes(filterName.toLowerCase()))
     let countryDisplay = <div></div>
-
+    
     if(countryList.length >= 10){
         countryDisplay = <div>Too many maches, specify another filter</div>
     }
@@ -19,11 +37,12 @@ const App = () =>{
                 <h1>{countryList[0].name}</h1>
                 <p>capital {countryList[0].capital}</p>
                 <p>population {countryList[0].population}</p>
-                <h2>languages</h2>
+                <h2>Spoken languages</h2>
                 <ul>
-                    {countryList[0].languages.map(val => <li>{val.name}</li>)}
+                    {countryList[0].languages.map(val => <li key={val.name}>{val.name}</li>)}
                 </ul>
-                <img width={100} src={countryList[0].flag} />
+                <img width={120} src={countryList[0].flag} alt={countryList[0].name} />
+                <Weather city={countryList[0].capital} />
             </div>
         )
     }
