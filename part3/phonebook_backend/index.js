bbef47197033f547822e9b0fd3808be5004060c0
artun-akdogan <artun.akdogan@boun.edu.tr>
2020-05-16 01:10:00 +0300

    Add code for exercise 3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -33,6 +33,16 @@ app.get('/info', (req, res) =>{
     res.send(`<p>Phonebook has info for ${persons.length} people<br><br>${Date()}`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id == id)
+    if(person){
+        res.json(person)
+    }else{
+        res.status(404).end()
+    }
+})
+
 const port = 3001
 app.listen(port)
 console.log("Server running on port", port)
